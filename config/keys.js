c4f5f54042c0c4faf9a195b4bf6b200b0e10fd80
monahvi <nahvi.moghaddam@gmail.com>
2023-09-24 21:20:58 +0100

    user collection

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  googleClientID: '',
-  googleClientSecret: '',
+  googleClientID: '176560023192-o5nhfq7e0ae4ns5tug0ftaiia6fp9875.apps.googleusercontent.com',
+  googleClientSecret: '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',
   mongoURI: '',
 };
